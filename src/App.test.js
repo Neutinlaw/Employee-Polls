1bd62f582ad81382be4d94776c57d9a17ed384d9
Tungcongau <Tungcongau1994@gmail.com>
2024-08-05 15:28:03 +0700

    Fix bug test script

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import configureStore from "redux-mock-store";
+import { thunk } from "redux-thunk";
+import * as ReactRedux from "react-redux";
 import App from "./App";
 
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useDispatch: jest.fn(),
+  useSelector: jest.fn(),
+}));
 jest.mock("./PageQuestions/PageQuestions", () => () => (
   <div>Page Questions</div>
 ));
 jest.mock("./LoginPage/LoginPage", () => () => <div>Login Page</div>);
 
 describe("App", () => {
+  let store;
+
   const mockSessionStorage = (isAuthenticated) => {
     Object.defineProperty(window, "sessionStorage", {
       value: {
@@ -22,6 +35,11 @@ describe("App", () => {
   };
 
   beforeEach(() => {
+    store = mockStore({});
+    jest.spyOn(ReactRedux, "useDispatch").mockReturnValue(jest.fn());
+  });
+
+  afterEach(() => {
     jest.clearAllMocks();
   });
 
